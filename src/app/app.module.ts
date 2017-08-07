b73780bd1f6a2bf65e0432a3cdd81b1d929889d0
Pubudu Ranasinghe <pubudutr@gmail.com>
2017-08-07 16:04:37 +0530

    block list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlockListComponent } from './block-list/block-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockListComponent
   ],
   imports: [
     BrowserModule
