0e7cb44fbf20823396fe420e75963eeaafdb6617
murtazasmart <murtazasmart@hotmail.com>
2017-08-17 11:12:41 +0530

    merge conflicts sorted

--- src/app/shared/dbservice.service.ts
+++ src/app/shared/dbservice.service.ts
@@ -7,16 +7,13 @@ export class DbserviceService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  // public get(): Promise<any> {
-  //   return new Promise(resolve => {
-  //     this.db.list('/transactions', {
-  //       query: {
-  //         orderByChild: 'hash',
-  //         equalTo: '0x952ae3686cee3660a84363b2c4740b0762a4c3f846f6c614dd8d4a77e2e6b365'
-  //       }
-  //     }).subscribe(data => {
-  //       console.log(data);
-  //     })
-  //   })
+  // public get(): Promise<number> {
+  //   // return new Promise(resolve => {
+  //   //   this.web3Service.connect((eer, web3) => {
+  //   //     web3.eth.getBlockNumber((err, num) => {
+  //   //       resolve(num);
+  //   //     })
+  //   //   })
+  //   // })
 
-  }
+}
