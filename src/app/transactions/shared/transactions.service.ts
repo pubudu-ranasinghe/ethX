d027f411d4f6a68392487d9e9c2204f97c9a8ae8
Achala Dissanayake <aijdissanayake@gmail.com>
2017-08-17 08:25:08 +0530

    transacion display

--- src/app/transactions/shared/transactions.service.ts
+++ src/app/transactions/shared/transactions.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Block } from "../../blocks/shared/block.model";
 import { Transaction } from "./transaction.model";
 import { Web3Service } from "./../../shared/web3.service";
-import { BlocksService } from "./../../blocks/shared/blocks.service";
-import { Block } from "./../../blocks/shared/block.model";
 
 @Injectable()
 export class TransactionsService {
-  private blocks:Array<Block> = [];
-  private txnList:Array<string> = [];
-  private txns:Array<Transaction> = [];
 
   constructor(
-    private web3Service: Web3Service,
-    private blocksService: BlocksService) { }
+    private web3Service: Web3Service) { }
 
   private createTxnFromResponse(response: any): Transaction {
     return new Transaction(
@@ -26,6 +21,46 @@ export class TransactionsService {
       response.gas,
       response.input
     )
+  }
+
+  getTransaction(id: string): Promise<Transaction> { 
+     return new Promise(resolve => {
+       console.log("inside");
+      this.web3Service.connect((err, web3) => {
+        web3.eth.getTransaction(id, (err, result) => {
+          var t = new Transaction(
+
+            result.hash,
+            result.nonce,
+            result.blockHash,
+            result.from,
+            result.to,
+            result.value,
+            result.gasPrice,
+            result.gas,
+            result.input
+        );
+          resolve(t);
+        })
+      })
+    })
+//     return new Promise(resolve => {
+//       this.web3Service.connect((err, web3) => {
+//         web3.eth.getBlock(20289, (err, result) => {
+// console.log("block");
+//           console.log(result);
+//           var b = new Block(
+//             result.number,
+//             result.timestamp,
+//             result.transactions.length,
+//             result.miner,
+//             result.hash,
+//             result.size,
+//             result.transactions);
+//           resolve(b);
+//         })
+//       })
+//     })
   }
  
 }
--- src/app/transactions/transaction/transaction.component.ts
+++ src/app/transactions/transaction/transaction.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Location } from "@angular/common";
+import { TransactionsService } from "./../shared/transactions.service";
+import "rxjs/add/operator/switchMap";
+import { Transaction } from "./../shared/transaction.model";
+
 
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.scss']
+  styleUrls: ['./transaction.component.scss'],
+  providers: [TransactionsService]
 })
 export class TransactionComponent implements OnInit {
+  transaction: Transaction;
 
-  constructor() { }
+  constructor(
+  	private route: ActivatedRoute,
+    private location: Location,
+    private transacionsService: TransactionsService
+    ) { }
 
   ngOnInit() { 
+
+  	this.route.paramMap
+      .switchMap((params: ParamMap) => this.transacionsService.getTransaction(params.get('id')).then(function(transaction){return transaction;}))
+      .subscribe(transaction => {
+        // checkTransacion(transaction).bind(this);
+        this.transaction = transaction;
+        console.log(this.transaction);
+        //alert(this.transaction);
+      });
   }
 
+}
+
+function checkTransacion(transaction) {
+    if(transaction == undefined) {
+       window.setTimeout(checkTransacion, 100); /* this checks the transaction every 100 milliseconds*/
+    } else {
+      this.transaction = transaction;
+      return transaction;
+    }
 }
